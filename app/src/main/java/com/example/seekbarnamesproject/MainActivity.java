15fbe945437111b1bce7dcf1a4f70c5f38b35b0f
shlominave <s1170@motzkine.ort.org.il>
2020-11-13 17:30:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/seekbarnamesproject/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.seekbarnamesproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+TextView tv1;
+SeekBar sk;
+Random rnd=new Random();
+String []names;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        names=new String[] {"adam","david","ron","shlomi","zion","youssef","micheal","jonathan","john","oz"};
+
+        tv1=findViewById(R.id.textView);
+        sk=findViewById(R.id.seekBar);
+        sk.setOnSeekBarChangeListener(sklistener);
+
+
+
+    }
+    private SeekBar.OnSeekBarChangeListener sklistener=new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+          int num=rnd.nextInt(names.length);
+          tv1.setText(names[num]);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+}
